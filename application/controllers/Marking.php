064b4c58f20f8097901bff9f0e413e0f772c01a7
kaichiuwong <kaichiuw.wong@utas.edu.au>
2019-12-27 17:59:56 +1100

    Implementation of Group Marking

--- application/controllers/Marking.php
+++ application/controllers/Marking.php
@@ -8,11 +8,22 @@ class Marking extends MY_PasController {
         $this->load->model('Assignment_mark_model');
     } 
 
-
-    function group()
+    function group($unit_code = null)
     {
         if ($this->check_permission(20) ) {
-
+            $this->load->model('Assignment_model');
+            if ($unit_code) {
+                $data['assignments'] = $this->Assignment_model->get_all_assignments_by_unit($unit_code);
+                $data['unit'] = strtoupper($unit_code);
+            }
+            else {
+                $data['assignments'] = $this->Assignment_model->get_all_assignments();
+                $data['unit'] = null;
+            }
+            $data['_view'] = 'pages/assignment_marking/group';
+            $this->load_header($data);
+            $this->load->view('templates/main',$data);
+            $this->load_footer($data);
         }
     } 
 
---
+++ application/views/pages/assignment_marking/group.php
@@ -0,0 +1,55 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1><?php echo ($unit)?$unit." - " : ""; ?>Assignment Management</h1>
+      </div>
+    </div>
+  </div><!-- /.container-fluid -->
+</section>
+
+<div class="row">
+  <div class="col-12">
+    <div class="card card-secondary">
+      <div class="card-header">
+        <h3 class="card-title">Assignment List</h3>
+      </div>
+      <!-- /.card-header -->
+      <div class="card-body table-responsive p-3">
+      <a href="<?php echo site_url('Assignmentadmin/add'); ?>" class="btn btn-success btn-sm">Create Assignment</a> 
+        <table class="table table-head-fixed table-hover enable-datatable">
+            <thead>
+                <tr>
+                    <th>Unit</th>
+                    <th>Semester</th>
+                    <th>Title</th>
+                    <th>Type</th>
+                    <th>Group Count</th>
+                    <th>Enrolled Students</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach($assignments as $a):?>
+                <tr>
+                    <td><?php echo $a['unit_code']; ?></td>
+                    <td><?php echo $a['sem']; ?></td>
+                    <td><?php echo $a['title']; ?></td>
+                    <td><?php echo $a['type'] ? "Group Assignment" : "Individual Assignment"; ?></td>
+                    <td><?php echo $a['topic_count']; ?></td>
+                    <td><?php echo $a['student_count']; ?></td>
+                    <td>
+                        <a href="<?php echo site_url('Marking/group/'.$a['id']); ?>" class="btn btn-primary btn-sm">Select</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+  </div>
+</div>
+<!-- /.row -->
