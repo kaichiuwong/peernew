29f8c44113a6c5ccc30401d983ab84974c37fc43
kaichiuwong <kaichiuw.wong@utas.edu.au>
2019-12-29 18:38:46 +1100

    Add the assignment score on the group page

--- application/views/pages/assignment_marking/group_submission.php
+++ application/views/pages/assignment_marking/group_submission.php
@@ -25,26 +25,38 @@
                     <th>Submission</th>
                     <th>Submitted by</th>
                     <th>Submission Time</th>
+                    <th style="width: 70px;">Score</th>
                     <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach($assignment_topics as $a){ ?>
                 <tr>
+                  <?php echo form_open('Marking/save/',array("class"=>"form-horizontal")); ?>
                     <td><a href="javascript:void(0);" data-grp-name="<?php echo $a['topic']; ?>" data-href="<?php echo site_url('assignment_topic/topic_member/'.$a['topic_id']); ?>" class="group_info_open"><?php echo $a['topic']; ?></a></td>
                     <?php if (empty($a['filename'])): ?>
                       <td class="text-muted">No Submission for this group</td>
                       <td></td>
                       <td></td>
                       <td>
-                          <a href="<?php echo site_url('Marking/save/'.$asg_id.'/'.$a['topic_id']); ?>" class="btn btn-info btn-sm">Save</a> 
+                        <input type="hidden" name="asg_id" value="<?php echo $asg_id;?>" required/>
+                        <input type="hidden" name="topic_id" value="<?php echo $a['topic_id'];?>" required/>
+                        <input type="number" min="0" step="1" name="score" value="" class="form-control input-sm" id="score" required />
+                      </td>
+                      <td>
+                        <button type="submit" class="btn btn-info btn-sm">Save</a> 
                       </td>
                     <?php else: ?>
                       <td><a href="<?php echo base_url().$a['filename']; ?>" target="_blank"><i class="fas fa-file"></i> <?php echo basename($a['filename']); ?></a></td>
                       <td><i class="fas fa-user"></i> <?php echo $a['user_id']; ?></td>
                       <td><i class="fas fa-clock"></i> <?php echo $a['submission_date']; ?> <?php echo ( $a['submission_date'] > $asg_deadline['date_value'] ) ? "<span class='badge badge-danger'>Late Submission</span>": ""; ?> </td>
                       <td>
-                          <a href="<?php echo site_url('Marking/save/'.$asg_id.'/'.$a['topic_id']); ?>" class="btn btn-info btn-sm">Save</a> 
+                        <input type="hidden" name="asg_id" value="<?php echo $asg_id;?>" required/>
+                        <input type="hidden" name="topic_id" value="<?php echo $a['topic_id'];?>" required/>
+                        <input type="number" min="0" step="1" name="score" value="" class="form-control input-sm" id="score" required />
+                      </td>
+                      <td>
+                        <button type="submit" class="btn btn-info btn-sm">Save</a> 
                       </td>
                     <?php endif; ?>
                 </tr>
